35a613bbe20b8fd6ab803195ecd58e5a203aade3
luka <lukachikashvili2020@gmail.com>
2024-02-18 18:38:12 +0400

    created and designed header file

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import {Menu} from 'lucide-react';
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <div className="logo">
+          <h2>Forever+</h2>
+       </div>
+
+       <nav>
+          <ul>
+               <li><a href = "#">Home</a></li>
+               <li><a href = "#">About</a></li>
+               <li><a href = "#">Products</a></li>
+               <li><a href = "#">More</a></li>
+          </ul>
+       </nav>
+
+
+       <div className="icons">
+          <label>menu</label>
+         <Menu size={30}/>
+       </div>
+    </div>
+  )
+}
+
+export default Header
