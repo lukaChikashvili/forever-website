b2b1423f15a3a9ceeb94dedbff29b5ebef711ba8
luka <lukachikashvili2020@gmail.com>
2024-02-19 16:13:04 +0400

    added smooth scroll effect using lenis library

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SmoothScroll from "./components/SmoothScroll";
 
 
 
@@ -11,10 +12,11 @@ function App() {
   return (
 
     <div className="App">
-      
+      <SmoothScroll>
       <Header />
       <Hero />
       <About />
+      </SmoothScroll>
     
 
     </div>
---
+++ src/components/SmoothScroll.jsx
@@ -0,0 +1,29 @@
+import { ReactLenis } from '@studio-freight/react-lenis'
+import {gsap} from 'gsap';
+
+
+import React, { useEffect, useRef } from 'react'
+
+const SmoothScroll = ({children}) => {
+
+     const lenisRef = useRef()
+  
+     useEffect(() => {
+       function update(time) {
+         lenisRef.current?.lenis?.raf(time * 800)
+       }
+     
+       gsap.ticker.add(update)
+     
+       return () => {
+         gsap.ticker.remove(update)
+       }
+     })
+  return (
+     <ReactLenis root  ref={lenisRef} autoRaf={false}>
+     { children }
+   </ReactLenis>
+  )
+}
+
+export default SmoothScroll
