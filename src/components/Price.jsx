b45494c3b85c08c92a289b8f5097f110a895fed1
luka <lukachikashvili2020@gmail.com>
2024-02-20 12:37:10 +0400

    added membership price element structure

--- src/components/Price.jsx
+++ src/components/Price.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {DollarSign} from 'lucide-react';
 
 const Price = () => {
   return (
@@ -9,7 +10,41 @@ const Price = () => {
 
      <div className="plans">
           <div className="plan">
-               
+              <span>Forever free</span>
+            <DollarSign className='price-icon'/>
+            <ul>
+              <li>Age Restriction </li>
+              <li>Single Purchase Limit </li>
+              <li>Identity Verification </li>
+              <li>Non-Transferable </li>
+            </ul>
+               <button>Get free plan</button>
+          </div>
+
+
+          <div className="plan">
+              <span>Forever standard</span>
+            <DollarSign className='price-icon'/>
+            <ul>
+              <li>Exclusive Access </li>
+              <li>Product Updates </li>
+              <li>Member Discounts </li>
+              <li>Early Access to Events </li>
+            </ul>
+               <button>Get standard plan</button>
+          </div>
+
+
+          <div className="plan">
+              <span>Forever premium</span>
+            <DollarSign className='price-icon'/>
+            <ul>
+              <li>VIP Events Access </li>
+              <li>Limited Edition Collectibles</li>
+              <li>Customized Perfume Experience </li>
+              <li>Exclusive Discounts and Offers </li>
+            </ul>
+               <button>Get premium plan</button>
           </div>
      </div>
     </div>
